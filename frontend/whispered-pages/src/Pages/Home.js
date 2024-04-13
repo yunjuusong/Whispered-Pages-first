ccbe9a203182896ae11383d2859247fdf4043104
Yunju Song <gpyunju2004@gmail.com>
2024-04-13 16:15:56 -0700

    basic moon design

---
+++ frontend/whispered-pages/src/Pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Home.css'; // Import CSS file for styling
+
+function Home() {
+  return (
+    <div className="home-container">
+      <h1>Whispered Pages</h1>
+      <p>Start your reading adventure here!</p>
+      <br />
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default Home;
